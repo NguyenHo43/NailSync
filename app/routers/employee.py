5ab8cb361a4cad305e2a402e226e0d4acc68a8dd
Nguyen Ho <hothaonguyen2@gmail.com>
2026-06-08 15:20:44 -0500

    Add salary calculation endpoint

--- app/routers/employee.py
+++ app/routers/employee.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.employee import Employee
+from app.models.turn import Turn
 from app.schemas.employee import EmployeeCreate
+
 
 router = APIRouter()
 
@@ -18,6 +21,24 @@ def create_employee(employee: EmployeeCreate, db: Session = Depends(get_db)):
 def get_employee(db: Session = Depends(get_db)):
     return db.query(Employee).all()
     
-@router.get("/employees/{id}")
-def get_employee_by_id(id: int, db: Session = Depends(get_db)):
+@router.get("/employees/{employee_id}")
+def get_employee_by_id(employee_id: int, db: Session = Depends(get_db)):
     return db.query(Employee).filter(Employee.id==id).first()
+
+@router.get("/employees/{employees_id}/salary")
+def get_salary(employee_id: int, month: int, year: int,db: Session = Depends(get_db)):
+    turns = db.query(Turn).filter(Turn.employee_id == employee_id,
+                                  func.extract('month', Turn.date)==month,
+                                  func.extract('year', Turn.date)==year).all()
+    
+    total_service = sum(turn.total_service for turn in turns)
+    total_tip = sum(turn.total_tip for turn in turns)
+
+    return {
+        "employee_id": employee_id,
+        "month": month,
+        "year": year,
+        "total_service": total_service,
+        "total_tip": total_tip,
+        "total": total_service + total_tip
+    }
