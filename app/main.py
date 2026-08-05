da90cadd4e8ac8071e6500fee78fd949e205c3a0
Nguyen Ho <hothaonguyen2@gmail.com>
2026-08-05 17:50:38 -0500

    Add production frontend origin to CORS config

--- app/main.py
+++ app/main.py
@@ -23,7 +23,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://nailsync-1.onrender.com/"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
