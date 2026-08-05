524fbe3dd8093ba4e29b165c3a0b2ad9445616ce
Nguyen Ho <hothaonguyen2@gmail.com>
2026-08-05 17:59:40 -0500

    Fix trailing slash in CORS origin

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://nailsync-1.onrender.com/"
+        "https://nailsync-1.onrender.com"
     ],
     allow_methods=["*"],
     allow_headers=["*"],
